a63cc03a76882bc76d292ef4bd91c3ccaae946c8
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-24 23:35:38 +0530

    [Sunny] Add : Create EmpWage Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,20 +1,22 @@
-const Is_Fulltime=2;
-const Is_Parttime=1;
-const Parttime_Hours=4;
-const Fulltime_Hours=8;
-const Wage_Per_Hours=20;
+const PartTime = 1;
+const FullTime = 2;
+const PartTimeHours = 4;
+const FullTimeHours = 8;
+const WageperHour = 20;
+
+function CalculateDailyWage(CheckEmp)
 {
-    let empHrs = 0;
-    let empCheck = Math.floor(Math.random()*10)%2;
-    switch(empCheck){
-        case Is_Parttime:
-            empHrs=Parttime_Hours;
-            break;
-        case Is_Fulltime:
-           empHrs=Fulltime_Hours;
-        default:
-            empHrs=0;
-    }
-    let empWage = empHrs*Wage_Per_Hours;
-    console.log("calculate empwage: "+empWage);
+    switch(CheckEmp)
+{
+    case PartTime:
+        return PartTimeHours; 
+    case FullTime:
+        return FullTimeHours;
+    default:
+        return 0;
 }
+}
+let CheckEmp = Math.floor(Math.random() * 10) % 3;
+let Emphours = CalculateDailyWage(CheckEmp);
+let EmpWage = Emphours * WageperHour;
+console.log("Employee Wage is: " + EmpWage);
