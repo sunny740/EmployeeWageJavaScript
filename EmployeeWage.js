b938975989c930ad29479209203fd3bedf52e967
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-25 00:39:46 +0530

    [Sunny] Add : EmpWage Using Arrow Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -10,5 +10,25 @@ else
     console.log("Employee is Absent");
 }
 
-//UC8- Map function
-console.log("EmpWage map total hours: "+Array.from(empDailyWageMap.values).reduce(totalWages,0));
+//UC9 Arrow function
+
+const findTotal = (totalVal,dailyVal) =>{
+    return totalVal+dailyVal;
+  }
+  
+  let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+  let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage>0).reduce(findTotal,0);
+  console.log("empwage with arrow "+ "Total Hours: "+totalHours+" total wages: "+ totalSalary);
+  
+  let nonWorkingDays = new Array();
+  let partWorkingDays = new Array();
+  let fullWorkingDays = new Array();
+  empDailyHrsMap.forEach((value, key, map) =>{
+      if(value==8) fullWorkingDays.push(key);
+      else if(value==4) partWorkingDays.push(key);
+      else nonWorkingDays.push(key);
+  });
+  
+  console.log("full working days: "+fullWorkingDays);
+  console.log("Part working days: "+partWorkingDays);
+  console.log("Non working days: "+nonWorkingDays);
