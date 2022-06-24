e9d9307e830aac73db270f1f92221e08c81851db
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-24 23:22:41 +0530

    [Sunny] Add : Check Employee Present or Not

---
+++ EmployeeWage.js
@@ -0,0 +1,11 @@
+const Is_Present=1
+let empCheck = Math.floor(Math.random()*10)%2;
+
+if(empCheck==Is_Present)
+{
+    console.log("employee is  Present");
+}
+else
+{
+    console.log("employee is Absent");
+}
