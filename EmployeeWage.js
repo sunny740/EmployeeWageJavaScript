e7e036c680b13c62bf33368d740db1e68138dad2
Sunny <sunnysejwarvitm005@gmail.com>
2022-07-02 15:32:03 +0530

    [Sunny] Add : Ability To Using object and Arrow Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -33,3 +33,37 @@ empDailyHrsAndWageArr.push(
 );
 }
 console.log("showing daily hours worked and wage earned: "+empDailyHrsAndWageArr);
+// ----------------------------------------------------------------------------------
+{
+    console.log("showing daily hours worked and wage earned: "+empDailyHrsAndWageArr);
+
+//UC 11A to 11D Using Object Functions Along With Arrow Functions
+let totalWages=empDailyHrsAndWageArr
+.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+.reduce((totalWage,dailyHrsAndWage) =>totalWage += dailyHrsAndWage.dailyWage,0);
+
+ // 11a: Calculate total Wage and total hours worked
+let totalHours=empDailyHrsAndWageArr
+.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+.reduce((totalHours,dailyHrsAndWage) =>totalHours += dailyHrsAndWage.dailyHours,0);
+console.log("\nUC 11A Total Hours: "+totalHours+"Total Wages: "+totalWages);
+
+
+    // 11b: Show the full workings days using foreach
+process.stdout.write("\nUC 11B Logging Full Work Days")
+empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
+      .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
+
+          // 11c: Show Part working days using Map by reducing to String Array
+let partWorkingDayStrArr = empDailyHrsAndWageArr
+            .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+            .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+console.log("\nUC 11C PartWorkingDayStrings: "+partWorkingDayStrArr);
+
+
+    // 11d: No working days only using Map function
+let nonWorkingDayNum=empDailyHrsAndWageArr
+        .filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours==0)
+        .map(dailyHrsAndWage=>dailyHrsAndWage.dayNum);
+console.log("\nUC 11D NonWorkingDayNums: "+nonWorkingDayNum);
+}
